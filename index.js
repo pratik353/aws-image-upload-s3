2870274b2dc1fb1034940dec9514e2cd63f52df3
Pratik Kamble <kamblepratik1137@gmail.com>
2024-06-06 12:44:00 +0530

    implemented get s3 file with presigned URL

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ import bodyParser from "body-parser";
 import cors from "cors";
 
 const s3 = new aws.S3({
-  region: process.env.REGION,
-  accessKeyId: process.env.ACCESS_KEY_ID,
-  secretAccessKey: process.env.SECRET_KEY_ID,
-  signatureVersion: process.env.SIGNATURE_VERSIONING,
+  region: "",
+  accessKeyId: "",
+  secretAccessKey: "",
+  signatureVersion: "v4",
 });
 
 //image-upload-with-presigned-url
@@ -20,13 +20,48 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/image-url", async (req, res) => {
   const name = req.query.name;
+  if (!name) {
+    return res.status(411).json({ error: "file name required" });
+  }
+  try {
+    const params = {
+      Bucket: "",
+      Key: name,
+      Expires: 60,
+    };
+    const signedURL = await s3.getSignedUrlPromise("putObject", params);
+    return res.status(200).json({ url: signedURL });
+  } catch (error) {
+    console.log(error);
+    return res.status(403).json({ error: "error ocurred" });
+  }
+});
+
+app.get("/get-s3-file", async (req, res) => {
+    const name = req.query.name;
+  if (!name) {
+    return res.status(411).json({ error: "file name required" });
+  }
   const params = {
-    Bucket: "image-upload-with-presigned-url",
-    Key: name,
-    Expires: 60,
+    Bucket:"",
+    Key: name, // e.g., 'folder/subfolder/file.txt'
+    Expires: 60, // URL expiration time in seconds
   };
-  const signedURL = await s3.getSignedUrlPromise("putObject", params);
-  return res.status(200).json({ url: signedURL });
+
+  try {
+      // Generate a presigned URL
+      s3.getSignedUrl("getObject", params, (err, url) => {
+        if (err) {
+          console.log("Error generating presigned URL", err);
+          return;
+        }
+        console.log("Presigned URL:", url);
+        return res.status(200).json({ url: url });
+      });
+  } catch (error) {
+    console.log(error)
+    return res.status(403).json({ error: "error ocurred" });
+  }
 });
 
 app.listen(8000, () => console.log("server running"));
